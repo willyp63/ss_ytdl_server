a054512f7784dab0ffbe5b809709602c778b15cb
Wil Pirino <wpirino1@gmail.com>
2016-07-21 13:40:16 -0700

    cleaned up code

--- index.js
+++ index.js
@@ -4,27 +4,11 @@ var io = require('socket.io')(http);
 var ss = require('socket.io-stream');
 var ytdl = require('ytdl-core');
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/bundle.js', function (req, res) {
-  res.sendFile(__dirname + '/bundle.js');
-});
-
-app.get('/style.css', function (req, res) {
-  res.sendFile(__dirname + '/style.css');
-});
-
 io.on('connection', function(socket){
   ss(socket).on('download', function (stream, data) {
     const url = `https://www.youtube.com/watch?v=${data.ytid}`;
-    try {
-      console.log(`downloading video from url: ${url}`);
-      ytdl(url, {filter: "audioonly"}).pipe(stream);
-    } catch (err) {
-      socket.emit('download_error', err);
-    }
+    console.log(`downloading video from url: ${url}`);
+    ytdl(url, {filter: "audioonly"}).pipe(stream);
   });
 });
 
