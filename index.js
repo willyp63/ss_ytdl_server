ee3482e9c3cd1a23968ad7346e16e16041f5f2d4
Wil Pirino <wpirino1@gmail.com>
2016-07-21 11:54:51 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var ss = require('socket.io-stream');
+var ytdl = require('ytdl-core');
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/bundle.js', function (req, res) {
+  res.sendFile(__dirname + '/bundle.js');
+});
+
+app.get('/style.css', function (req, res) {
+  res.sendFile(__dirname + '/style.css');
+});
+
+io.on('connection', function(socket){
+  ss(socket).on('download', function (stream, data) {
+    const url = `https://www.youtube.com/watch?v=${data.ytid}`;
+    try {
+      console.log(`downloading video from url: ${url}`);
+      ytdl(url, {filter: "audioonly"}).pipe(stream);
+    } catch (err) {
+      socket.emit('download_error', err);
+    }
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
