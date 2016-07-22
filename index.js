11369c702a7f01a2d1b1d427f76021650df51416
Wil Pirino <wpirino1@gmail.com>
2016-07-21 20:08:05 -0700

    remove config

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ var io = require('socket.io')(http, {'transports': ['websocket', 'polling']});
 var ss = require('socket.io-stream');
 var ytdl = require('ytdl-core');
 
-io.configure(function () {
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 10);
-});
-
 io.set('origins', 'https://salty-falls-17641.herokuapp.com');
 
 io.on('connection', function(socket){
