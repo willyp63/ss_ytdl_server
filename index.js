38a095033ab0e2bf923f9a3582f0e39d2e3ebecd
Wil Pirino <wpirino1@gmail.com>
2016-07-21 19:51:47 -0700

    polling options

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {'transports': ['websocket', 'polling']});
 var ss = require('socket.io-stream');
 var ytdl = require('ytdl-core');
 
