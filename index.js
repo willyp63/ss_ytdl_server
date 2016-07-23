fbc622f4d0cf2a8ac913d5f10ebba5806d17f0b4
Wil Pirino <wpirino1@gmail.com>
2016-07-22 20:38:36 -0700

    added download tracking in console log

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var ss = require('socket.io-stream');
 var ytdl = require('ytdl-core');
+var WritableStream = require('stream').Writable;
 
 io.on('connection', function(socket){
   ss(socket).on('download', function (stream, data) {
+    // stream download
     const url = `https://www.youtube.com/watch?v=${data.ytid}`;
-    console.log(`downloading video from url: ${url}`);
-    ytdl(url, {filter: "audioonly"}).pipe(stream);
+    const downloadStream = ytdl(url, {filter: "audioonly"});
+    downloadStream.pipe(stream).on('finish', function () {
+      console.log(`Finished Downloading from URL:${url}`);
+    });
+
+    // track download
+    console.log(`Begun Downloading from URL:${url}`);
+    let chunkNum = 0;
+    const ws = new WritableStream();
+    ws._write = function (chunk, type, next) {
+      console.log(`Sent Chunk#${chunkNum++} from URL:${url}`);
+    };
+    downloadStream.pipe(ws);
   });
 });
 
+// listen on heroku port or 8080
 const port = process.env.PORT || 8080;
 http.listen(port, function () {
   console.log(`listening on *:${port}`);
