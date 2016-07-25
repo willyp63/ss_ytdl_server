c62989b56e7d32f2163e9731450a82bbe1812fdf
Wil Pirino <wpirino1@gmail.com>
2016-07-25 14:03:34 -0700

    cleaned up more

--- server.js
+++ server.js
@@ -3,29 +3,42 @@ var ytdl = require('ytdl-core');
 
 const port = process.env.PORT || 8080;
 http.createServer(function (req, res) {
-  const urlMatch = req.url.match(/^\/download\/(.*)$/);
-  if (urlMatch) {
-    const ytid = urlMatch[1];
-    const url = `https://www.youtube.com/watch?v=${ytid}`;
-    const downloadStream = ytdl(url, {filter: "audioonly"});
-    downloadStream.on('response', function (downloadRes) {
-      const total = downloadRes.headers['content-length'];
-      var range = req.headers.range;
-      var positions = range.replace(/bytes=/, "").split("-");
-      var start = parseInt(positions[0], 10);
-      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-      var chunksize = (end - start) + 1;
-      res.writeHead(206, {
-        "Content-Range": "bytes " + start + "-" + end + "/" + total,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunksize,
-        "Content-Type": "video/mp4"
-      });
-      downloadStream.pipe(res);
-    });
+  const ytid = matchUrl(req.url);
+  if (ytid) {
+    audioStream(ytid).on('response', function (downloadRes) {
+      res.writeHead(206, resHeader(req, downloadRes));
+    }).on('end', function () {
+      res.end();
+    }).pipe(res);
   } else {
-    res.sendStatus(404);
+    res.writeHead(404);
+    res.end();
   }
 }).listen(port, function () {
   console.log(`listening on *:${port}`);
 });
+
+function matchUrl (url) {
+  const match = url.match(/^\/download\/(.*)$/);
+  return match ? match[1] : null;
+}
+
+function audioStream (ytid) {
+  const url = `https://www.youtube.com/watch?v=${ytid}`;
+  return ytdl(url, {filter: "audioonly"});
+}
+
+function resHeader (req, downloadRes) {
+  const total = downloadRes.headers['content-length'];
+  var range = req.headers.range;
+  var positions = range.replace(/bytes=/, "").split("-");
+  var start = parseInt(positions[0], 10);
+  var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+  var chunksize = (end - start) + 1;
+  return {
+    "Content-Range": "bytes " + start + "-" + end + "/" + total,
+    "Accept-Ranges": "bytes",
+    "Content-Length": chunksize,
+    "Content-Type": "audio/mp3"
+  };
+}
