0cdbd3098e39401f34804d59a529ff9e504f7919
Wil Pirino <wpirino1@gmail.com>
2016-07-25 17:05:58 -0700

    accepts partial requests (range)

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 var http = require('http');
 var ytdl = require('ytdl-core');
 
+let _streamSizes = {};
+
 const port = process.env.PORT || 8080;
+
 http.createServer(function (req, res) {
   const ytid = matchUrl(req.url);
   if (ytid) {
-    console.log(`***Started Streaming Audio for ytid:${ytid}***`);
-    const stream = audioStream(ytid).on('response', function (downloadRes) {
-      res.writeHead(206, resHeader(req, downloadRes));
-      stream.pipe(res);
-    }).on('end', function () {
-      console.log(`***Finished Streaming Audio for ytid:${ytid}***`);
-      res.end();
-    });
+    if (initRequest(req)) {
+      // stream entire audio
+      const stream = audioStream(ytid, 0).on('response', function (downloadRes) {
+        // save stream size
+        _streamSizes[ytid] = downloadRes.headers['content-length'];
+
+        // stream audio
+        const reqRange = requestRange(req, _streamSizes[ytid]);
+        res.writeHead(206, responseHeader(reqRange, _streamSizes[ytid]));
+        stream.pipe(res);
+      }).on('end', function () {
+        res.end();
+      });
+    } else {
+      // stream only requested range
+      const reqRange = requestRange(req, _streamSizes[ytid]);
+      const stream = audioStream(ytid, reqRange.start, reqRange.end).on('response', function () {
+        // stream audio
+        res.writeHead(206, responseHeader(reqRange, _streamSizes[ytid]));
+        stream.pipe(res);
+      }).on('end', function () {
+        res.end();
+      });
+    }
   } else {
+    // not a valid url
     res.writeHead(404);
     res.end();
   }
@@ -26,22 +46,33 @@ function matchUrl (url) {
   return match ? match[1] : null;
 }
 
-function audioStream (ytid) {
+function audioStream (ytid, start, end) {
+  const range = end ? `${start}-${end}` : `${start}-`;
   const url = `https://www.youtube.com/watch?v=${ytid}`;
-  return ytdl(url, {filter: "audioonly"});
+  return ytdl(url, {filter: "audioonly", range: range});
 }
 
-function resHeader (req, downloadRes) {
-  const total = downloadRes.headers['content-length'];
+function initRequest (req) {
   var range = req.headers.range;
   var positions = range.replace(/bytes=/, "").split("-");
   var start = parseInt(positions[0], 10);
-  var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+  return !start;
+}
+
+function requestRange (req, totalBytes) {
+  var range = req.headers.range;
+  var positions = range.replace(/bytes=/, "").split("-");
+  var start = parseInt(positions[0], 10);
+  var end = positions[1] ? parseInt(positions[1], 10) : totalBytes - 1;
   var chunksize = (end - start) + 1;
+  return {start: start, end: end, chunksize: chunksize};
+}
+
+function responseHeader (reqRange, totalBytes) {
   return {
-    "Content-Range": "bytes " + start + "-" + end + "/" + total,
+    "Content-Range": "bytes " + reqRange.start + "-" + (reqRange.end - 1) + "/" + totalBytes,
     "Accept-Ranges": "bytes",
-    "Content-Length": chunksize,
+    "Content-Length": reqRange.chunksize,
     "Content-Type": "audio/mp3"
   };
 }
