4ea95f3c895950d32098b78ca7291b801bb16b3b
Wil Pirino <wpirino1@gmail.com>
2016-08-26 15:35:31 -0700

    stream both webm and mp4

--- server.js
+++ server.js
@@ -3,57 +3,79 @@
 const app = require('express')();
 const ytdl = require('ytdl-core');
 
+// YTID CACHE
 let _ytids = {};
-let _totalBytes = {};
 
-// ORIGINS
+// ALLOW ALL ORIGINS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-// ACCESS CACHE
+// READ CACHE
 app.get('/ytid/:spotifyId', function (req, res) {
+  const spotifyId = req.params.spotifyId;
   res.writeHead(200, {"Content-Type": "application/json"});
-  if (_ytids[req.params.spotifyId]) {
-    const url = `https://www.youtube.com/watch?v=${_ytids[req.params.spotifyId]}`;
-    const stream = ytdl(url, {filter: filterFormats}).on('info', function (info, format) {
+  console.log(`Checking Cache for SpotifyId! (${spotifyId})`);
+  if (_ytids[spotifyId]) {
+    const ytid = _ytids[spotifyId];
+    // check if stream is still there
+    const url = `https://www.youtube.com/watch?v=${ytid}`;
+    const stream = ytdl(url, {filter: 'audioonly'}).on('info', function (info, format) {
+      // end stream and return ytid
       stream.destroy();
-      res.end(JSON.stringify({ytid: _ytids[req.params.spotifyId]}));
+      console.log(`Successfully Returned YTID from Cache! (${ytid})`);
+      res.end(JSON.stringify({ytid: ytid}));
     }).on('error', function (err) {
-      // remove ytid from cache
-      _ytids[req.params.spotifyId] = null;
-      console.error(err.stack);
+      // remove ytid from cache and return null
+      _ytids[spotifyId] = null;
+      console.log(`Removed YTID from Cache B/C Stream Failed! (${ytid})`);
       res.end(JSON.stringify({ytid: null}));
     });
   } else {
+    console.log(`SpotifyId was not in Cache! (${spotifyId})`);
     res.end(JSON.stringify({ytid: null}));
   }
 });
 
-// ACCESS CACHE
+// WRITE CACHE
 app.get('/cache', function (req, res) {
+  // store ytid
   _ytids[req.query.spotifyId] = req.query.ytid;
+  console.log(`Stored YTID in Cache! (${req.query.spotifyId} - ${req.query.ytid})`);
   res.sendStatus(200);
 });
 
 // STREAM
-app.get('/stream/:ytid', function (req, res) {
-  // stream only requested range
+app.get('/stream', function (req, res) {
+  console.log('$$$$$$$$$$$$$$$$$$$$');
+  console.log(req.headers);
+  const ytid = req.query.ytid;
+  const encoding = req.query.encoding;
+  console.log(req.query.encoding);
   const reqRange = requestRange(req);
-  if (_totalBytes[req.params.ytid]) {
-    streamAudio(req.params.ytid, reqRange, _totalBytes[req.params.ytid], res);
+
+  // check if partial request goes to end of file
+  if (!reqRange.end) {
+    console.log('EOF Request!');
+    streamAudio(res, ytid, reqRange, encoding);
   } else {
-    getTotalBytes(req.params.ytid, function (totalBytes) {
-      streamAudio(req.params.ytid, reqRange, totalBytes, res);
+    // must get totalBytes with seperate request
+    console.log('NOT EOF Request!');
+    getTotalBytes(ytid, encoding, function (totalBytes) {
+      streamAudio(res, ytid, reqRange, encoding, totalBytes);
     });
   }
 });
 
-function streamAudio (ytid, reqRange, totalBytes, res) {
-  const stream = audioStream(ytid, reqRange.start, reqRange.end).on('response', function (downloadRes) {
+function streamAudio (res, ytid, reqRange, encoding, totalBytes) {
+  const stream = audioStream(ytid, encoding, reqRange.start, reqRange.end).on('response', function (downloadRes) {
+    totalBytes = (totalBytes || parseInt(downloadRes.headers['content-length']));
+    res.writeHead(206, responseHeader(reqRange, encoding, totalBytes));
     // stream audio
-    res.writeHead(206, responseHeader(reqRange, totalBytes));
+    console.log('%%%%%%%%%' + downloadRes.headers['content-type']);
+    console.log(responseHeader(reqRange, encoding, totalBytes));
+    console.log(`Streaming Audio for YTID! (${ytid} (${reqRange.start} - ${reqRange.end}))`);
     stream.pipe(res);
   }).on('error', function (err) {
     console.error(err.stack);
@@ -62,18 +84,20 @@ function streamAudio (ytid, reqRange, totalBytes, res) {
 }
 
 // AUDIO ENCODING
-app.get('/audioEncoding/:ytid', function (req, res) {
-  res.writeHead(200, {"Content-Type": "application/json"});
-  res.end(JSON.stringify({validFormat: true}));
-  // const url = `https://www.youtube.com/watch?v=${req.params.ytid}`;
-  // const stream = ytdl(url, {filter: filterFormats}).on('info', function (info, format) {
-  //   stream.destroy();
-  //   res.writeHead(200, {"Content-Type": "application/json"});
-  //   res.end(JSON.stringify({validFormat: (format.audioEncoding === 'aac' && format.container === 'mp4')}));
-  // }).on('error', function (err) {
-  //   console.error(err.stack);
-  //   res.status(500).send('Can not get Stream Info!');
-  // });
+app.get('/audioEncoding', function (req, res) {
+  const url = `https://www.youtube.com/watch?v=${req.query.ytid}`;
+  const filterFunction = (req.query.encoding === 'opus' ? opusFormat : aacFormat);
+  console.log(req.query.encoding);
+  const stream = ytdl(url, {filter: filterFunction}).on('info', function (info, format) {
+    stream.destroy();
+    console.log('good');
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({validFormat: true}));
+  }).on('error', function (err) {
+    console.log('bad');
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({validFormat: false}));
+  });
 });
 
 // listen on heroku port or 8080
@@ -82,22 +106,24 @@ app.listen(port, function () {
   console.log(`listening on *:${port}`);
 });
 
-function getTotalBytes (ytid, returnTotalBytes) {
-  const stream = audioStream(ytid, 0, null).on('response', function (downloadRes) {
-    _totalBytes[ytid] = parseInt(downloadRes.headers['content-length']);
-    returnTotalBytes(_totalBytes[ytid]);
+function getTotalBytes (ytid, encoding, returnTotalBytes) {
+  const stream = audioStream(ytid, encoding, 0, null).on('response', function (downloadRes) {
     stream.destroy();
+    returnTotalBytes(parseInt(downloadRes.headers['content-length']));
   }).on('error', function (err) {
     console.error(err.stack);
   });
 }
 
-function audioStream (ytid, start, end) {
+// return a YT stream
+function audioStream (ytid, encoding, start, end) {
+  const filterFunction = (encoding === 'opus' ? opusFormat : aacFormat);
   const range = end ? `${start}-${end}` : `${start}-`;
   const url = `https://www.youtube.com/watch?v=${ytid}`;
-  return ytdl(url, {filter: filterFormats, range: range});
+  return ytdl(url, {filter: filterFunction, range: range});
 }
 
+// parse req headers to get byte range
 function requestRange (req) {
   const range = req.headers.range;
   if (!range) { return {start: 0, end: null}; }
@@ -107,16 +133,25 @@ function requestRange (req) {
   return {start: start, end: end};
 }
 
-function responseHeader (reqRange, totalBytes) {
+// return response headers for partial content
+function responseHeader (reqRange, encoding, totalBytes) {
   const end = reqRange.end || totalBytes - 1;
   return {
-    "Content-Range": "bytes " + reqRange.start + "-" + (end) + "/" + totalBytes,
-    "Accept-Ranges": "bytes",
-    "Content-Length": (end - reqRange.start + 1),
-    "Content-Type": "audio/mp4"
+    "content-range": "bytes " + reqRange.start + "-" + (end) + "/" + totalBytes,
+    "accept-ranges": "bytes",
+    "content-length": (end - reqRange.start + 1),
+    "content-type": (encoding === 'opus' ? 'audio/webm' : 'audio/mp4')
   };
 }
 
-function filterFormats (format) {
-  return (format.audioEncoding === 'aac' && format.container === 'mp4');
+function aacFormat (format) {
+  return (format.resolution === '360p' &&
+          format.container === 'mp4' &&
+          format.audioEncoding === 'aac');
+}
+
+function opusFormat (format) {
+  return (format.resolution === null &&
+          format.container === 'webm' &&
+          format.audioEncoding === 'opus');
 }
